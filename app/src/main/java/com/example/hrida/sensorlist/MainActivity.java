abca1685a69d2e5a424f5b6fc55b27c2af92023e
HRida <H.Rida1@hotmail.com>
2019-01-17 21:21:30 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hrida/sensorlist/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.hrida.sensorlist;
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv1 = null;
+    private SensorManager mSensorManger;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv1 = (TextView) findViewById(R.id.textView2);
+        tv1.setVisibility(View.GONE);
+
+        mSensorManger = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> mList = mSensorManger.getSensorList(Sensor.TYPE_ALL);
+
+        for(int i = 1; i < mList.size(); i++) {
+            tv1.setVisibility(View.VISIBLE);
+            tv1.append("\n" + mList.get(i).getName() + "\n" + mList.get(i).getVendor() + "\n" + mList.get(i).getVersion());
+        }
+    }
+}
